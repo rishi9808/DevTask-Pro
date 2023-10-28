c8162a706beabdca0dad26794248cc2424b513ed
Rishikesh K V <rishikeshkaroth@gmail.com>
2023-10-28 23:43:35 +0530

    refactored code

--- client/src/pages/Admin/adEmployee.jsx
+++ client/src/pages/Admin/adEmployee.jsx
@@ -1,76 +1,74 @@
-import '../../styles/UserHome.css'
-import { useState,useEffect } from 'react';
-import axios from 'axios';
-import NavbarJm from '../../Components/Admin/NavbarJm.jsx'
-function ShowEmployee() { // need postedjob id and a functioon to close this popup
+import "../../styles/UserHome.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import NavbarJm from "../../Components/Admin/NavbarJm.jsx";
+function ShowEmployee() {
+  // need postedjob id and a functioon to close this popup
   const [Appls, setAppls] = useState([]);
   async function fetchAppl() {
-    const response = await axios.get(`https://devtask-pro.onrender.com/getusers`)
+    const response = await axios.get(
+      `https://devtask-pro.onrender.com/getusers`
+    );
     setAppls(response.data);
-    console.log("here is the application data")
-    console.log(response.data)
+    console.log("here is the application data");
+    console.log(response.data);
   }
-  useEffect((function () {
+  useEffect(function () {
     fetchAppl();
-  }), []);
-  
- 
+  }, []);
+
   function getSkills(skills) {
-    const filteredSkills = [...new Set(skills.filter(skill => skill !== "None"))];
-    console.log("The skills are" + filteredSkills.join(', '))
-    return filteredSkills.join(',');
+    const filteredSkills = [
+      ...new Set(skills.filter((skill) => skill !== "None")),
+    ];
+    console.log("The skills are" + filteredSkills.join(", "));
+    return filteredSkills.join(",");
   }
-  function getAvailability(avail){
-    if(avail){
-        return "Yes"
-    }
-    else{
-        return "No"
+  function getAvailability(avail) {
+    if (avail) {
+      return "Yes";
+    } else {
+      return "No";
     }
   }
 
   return (
     <>
-    <NavbarJm  />
-    <div className='active main-content m-5 rounded-bottom'>
-      <div className="popup-content main-content">
-        <h3>Employee Status</h3>
-        <table style={{ borderSpacing:'20px', borderCollapse:"separate" }}>
-          <thead>
-            <tr>
-              <th>Sl. No.</th>
-              <th >Employee  Name</th>
-              <th> </th>
-              <th>Availability</th>
-              <th>Skills</th>
-              <th>Finished Tasks</th>
-              <hr />
-            </tr>
-          </thead>
-          <tbody>
-            {Appls.map((Appl, index) => (
-              <tr key={index}>
-                <td>{index + 1}</td>
-              
-                <td>{Appl.fullName}</td>
-                <td>   </td>
-                <td>{getAvailability(Appl.availStatus)}</td>
-                <td>{getSkills(Appl.skills)}</td>
-                <td>{Appl.jobdone}</td>
+      <NavbarJm />
+      <div className="active main-content m-5 rounded-bottom">
+        <div className="popup-content main-content">
+          <h3>Employee Status</h3>
+          <table style={{ borderSpacing: "20px", borderCollapse: "separate" }}>
+            <thead>
+              <tr>
+                <th>Sl. No.</th>
+                <th>Employee Name</th>
+                <th> </th>
+                <th>Availability</th>
+                <th>Skills</th>
+                <th>Finished Tasks</th>
                 <hr />
               </tr>
-            
-            ))}
+            </thead>
+            <tbody>
+              {Appls.map((Appl, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
 
-          </tbody>
-
-        </table>
+                  <td>{Appl.fullName}</td>
+                  <td> </td>
+                  <td>{getAvailability(Appl.availStatus)}</td>
+                  <td>{getSkills(Appl.skills)}</td>
+                  <td>{Appl.jobdone}</td>
+                  <hr />
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
     </>
-
-    
-  )
+  );
 }
 
 export { ShowEmployee };
